ec0cb37cbf08eaa545e629382954c71a77bf43bd
perezhernandezjazziel183@gmail.com <perezhernandezjazziel183@gmail.com>
2025-07-07 12:38:00 -0600

    Primer commit

---
+++ src/main/java/com/punto/venta/PuntoVentas.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.punto.venta;
+
+
+/**
+ *
+ * @author Jazzi
+ */
+public class PuntoVentas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+    }
+}
